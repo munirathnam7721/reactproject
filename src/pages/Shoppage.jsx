0026a8e26c497f426ba99656428986437fe9eda8
muniram72071 <munirathnam7721@gmail.com>
2025-06-17 13:06:20 +0530

    Create Shop page  Successfully

---
+++ src/pages/Shoppage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ShopcartBanner from '../components/ShopcartBanner'
+import Searchbar from '../components/Searchbar'
+
+
+export default function Shoppage() {
+  return (
+    <div>
+        <ShopcartBanner/>
+        <Searchbar/>
+
+    </div>
+  )
+}
