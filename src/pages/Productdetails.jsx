27cf38869251555596c49eb0bb7c11f8bb6800af
muniram72071 <munirathnam7721@gmail.com>
2025-06-17 12:58:06 +0530

    Create Productdetails page  Successfully

---
+++ src/pages/Productdetails.jsx
@@ -0,0 +1,14 @@
+import DescriptionToggel from '../components/DescriptionToggel'
+import LikedProducts from '../components/LikedProducts'
+import Productdetailsbanner from '../components/Productdetailsbanner'
+
+export default function Productdetails() {
+  
+  return (
+    <div>
+    <Productdetailsbanner/>
+    <DescriptionToggel/>
+    <LikedProducts/>
+    </div>
+)
+}
